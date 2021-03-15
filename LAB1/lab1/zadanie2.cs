dd3b702c2841983def66f95fe883118b297988bc
Goose232 <oleinikoff.daniil2014@yandex.ru>
2021-03-15 12:48:38 +0300

    l1 added

---
+++ LAB1/lab1/zadanie2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+   static class Zadanie2
+   {
+      public static void z2() 
+      {
+            Console.WriteLine("Задание 2");
+
+            for (int j = 1900; j <= 2000; j++) // перебираем все года от 1900 до 2000
+            {
+                if (j % 4 == 0)
+                {
+                    Console.WriteLine(j + " - Високосный год"); // каждый 4 год - високосный 
+                }
+                else
+                {
+                    Console.WriteLine(j + " - Не високосный год");
+                }
+
+            }
+            Console.WriteLine("");
+      }
+   }
+}
